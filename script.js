fc48b944b719423bc3fe615d74f3b2405020e955
Ya2ro <ya2ro26@gmail.com>
2024-09-15 09:36:32 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Простая анимация для кнопок
-document.querySelectorAll('button, .cta-button').forEach(button => {
-  button.addEventListener('mouseenter', () => {
-    button.style.transform = 'scale(1.05)';
-  });
-  button.addEventListener('mouseleave', () => {
-    button.style.transform = 'scale(1)';
-  });
-});
